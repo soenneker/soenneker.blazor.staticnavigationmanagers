2f428e2b6af02804ca51fb44ad5cdfc0ac41a5c4
soenneker <jake@soenneker.com>
2026-08-29 14:16:50 -0500

    Improve public API XML documentation

--- src/Soenneker.Blazor.StaticNavigationManagers/Registrars/StaticNavigationManagerRegistrar.cs
+++ src/Soenneker.Blazor.StaticNavigationManagers/Registrars/StaticNavigationManagerRegistrar.cs
@@ -12,6 +12,8 @@ public static class StaticNavigationManagerRegistrar
     /// <summary>
     /// Adds <see cref="StaticNavigationManager"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddStaticNavigationManagerAsSingleton(this IServiceCollection services)
     {
         services.TryAddSingleton<NavigationManager>(_ => new StaticNavigationManager());
@@ -22,6 +24,8 @@ public static class StaticNavigationManagerRegistrar
     /// <summary>
     /// Adds <see cref="StaticNavigationManager"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddStaticNavigationManagerAsScoped(this IServiceCollection services)
     {
         services.TryAddScoped<NavigationManager>(_ => new StaticNavigationManager());
